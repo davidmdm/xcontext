e99e4f91689cafa9a4951a74348a9464d16b1a0b
davidmdm <desmarais.michaud.david@gmail.com>
2023-10-21 22:35:45 -0400

    generate textfmt coverage report

--- coverage-gocoverdir.sh
+++ coverage-gocoverdir.sh
@@ -1,3 +1,4 @@
 rm -rf ./coverage && mkdir ./coverage
 go test -count 1 -cover -test.gocoverdir=./coverage ./...
 go tool covdata percent -i ./coverage
+go tool covdata textfmt -i ./coverage -o ./coverage.out
